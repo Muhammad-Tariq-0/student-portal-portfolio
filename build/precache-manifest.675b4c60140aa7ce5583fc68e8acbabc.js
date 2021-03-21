3734cb1978b2e7c90c906068c68059b8f29c8886
Muhammad Tariq <muhammad64tariq@gmail.com>
2021-03-21 20:05:17 +0500

    update

---
+++ build/precache-manifest.675b4c60140aa7ce5583fc68e8acbabc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d231acd333ce84bc8dc2c4babef2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10058d5207e4114388eb",
+    "url": "/static/css/2.28f57148.chunk.css"
+  },
+  {
+    "revision": "8fab538c943247e2359f",
+    "url": "/static/css/main.fa8869ee.chunk.css"
+  },
+  {
+    "revision": "10058d5207e4114388eb",
+    "url": "/static/js/2.0e25e760.chunk.js"
+  },
+  {
+    "revision": "c457a2cc2667e15e709bd3463b336807",
+    "url": "/static/js/2.0e25e760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fab538c943247e2359f",
+    "url": "/static/js/main.777adb32.chunk.js"
+  },
+  {
+    "revision": "9a17e8f4311681ecec55",
+    "url": "/static/js/runtime-main.8fd531e0.js"
+  },
+  {
+    "revision": "4744a36f4a0feaffe40b9ff7a700a1e2",
+    "url": "/static/media/book1.4744a36f.png"
+  },
+  {
+    "revision": "de251820c3d84bbf0d53708b3742c495",
+    "url": "/static/media/final-uot.de251820.gif"
+  },
+  {
+    "revision": "be833fc19e648f60b7140af9efa83a2e",
+    "url": "/static/media/program.be833fc1.jpg"
+  },
+  {
+    "revision": "ccc106dff913e0b00e3254ab4af7a957",
+    "url": "/static/media/uot.ccc106df.png"
+  }
+]);
